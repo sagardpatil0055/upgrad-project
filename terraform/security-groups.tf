ba0208051e3fc43d9369d62584d44b2337f08c18
sagardpatil0055 <sagardpatil0055@gmail.com>
2025-07-19 18:43:15 +0530

    Update security-groups.tf

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "public_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = "123.45.67.89/32"]
   }
 
   egress {

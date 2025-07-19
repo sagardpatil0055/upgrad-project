c35e26e498647d654967cefe45c2618b8924e5c4
sagardpatil0055 <sagardpatil0055@gmail.com>
2025-07-19 19:11:07 +0530

    Update instances.tf

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "bastion" {
   ami                         = "ami-020cba7c55df1f615"
-  instance_type               = "t2.micro"
+  instance_type               = "t2.small"
   subnet_id                   = aws_subnet.public[0].id
   vpc_security_group_ids      = [aws_security_group.public_sg.id]
   associate_public_ip_address = true
@@ -13,7 +13,7 @@ resource "aws_instance" "bastion" {
 
 resource "aws_instance" "app" {
   ami                    = "ami-020cba7c55df1f615"
-  instance_type          = "t2.micro"
+  instance_type          = "t2.small"
   subnet_id              = aws_subnet.private[0].id
   vpc_security_group_ids = [aws_security_group.private_sg.id]
   key_name               = "project"
@@ -24,7 +24,7 @@ resource "aws_instance" "app" {
 
 resource "aws_instance" "jenkins" {
   ami                    = "ami-020cba7c55df1f615"
-  instance_type          = "t2.micro"
+  instance_type          = "t2.small"
   subnet_id              = aws_subnet.private[1].id
   vpc_security_group_ids = [aws_security_group.private_sg.id]
   key_name               = "project"

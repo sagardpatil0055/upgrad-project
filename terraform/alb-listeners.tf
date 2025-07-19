ca51fe5ffc8956b3a8c8d0db959f60531906b51d
sagardpatil0055 <sagardpatil0055@gmail.com>
2025-07-19 18:59:48 +0530

    Create alb-listeners.tf

---
+++ terraform/alb-listeners.tf
@@ -0,0 +1,30 @@
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.sagar_alb.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default response on 8080"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "jenkins_rule" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.jenkins_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/jenkins*", "/login*"]
+    }
+  }
+}

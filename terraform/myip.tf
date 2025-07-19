c0a18075226c4011f36d886dbff3dda7e77c5276
sagardpatil0055 <sagardpatil0055@gmail.com>
2025-07-19 20:15:21 +0530

    Create myip.tf

---
+++ terraform/myip.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Get your public IP from ipify.org
+
+data "http" "my_ip" {
+  url = "https://api.ipify.org"
+}
+
+# Format it to CIDR
+
+locals {
+  my_ip_cidr = "${trimspace(data.http.my_ip.response_body)}/32"
+}
+
+
+ 
+    cidr_blocks = [local.my_ip_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "public-sg"
+  }
+}

e0b0ed4e414550fe603dd776f221f7324937ce8e
sagardpatil0055 <sagardpatil0055@gmail.com>
2025-07-22 20:09:37 +0530

    Update alb-listeners.tf

--- terraform/alb-listeners.tf
+++ terraform/alb-listeners.tf
@@ -60,3 +60,20 @@ resource "aws_lb_listener_rule" "node_rule" {
     }
   }
 }
+
+resource "aws_lb_listener_rule" "result_rule" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 103  # Ensure this is unique
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.result_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/result*"]
+    }
+  }
+}
+
